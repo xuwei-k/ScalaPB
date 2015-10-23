af6bb7ddef1c225edf59f6d36dba7190999cdf6a
xuwei-k <6b656e6a69@gmail.com>
2015-10-24 01:01:55 +0900

    flush

--- compiler-plugin/src/main/scala/com/trueaccord/scalapb/compiler/Main.scala
+++ compiler-plugin/src/main/scala/com/trueaccord/scalapb/compiler/Main.scala
@@ -3,4 +3,5 @@ package com.trueaccord.scalapb.compiler
 object Main extends App {
   val response = Process.runWithInputStream(System.in)
   System.out.write(response.toByteArray)
+  System.out.flush()
 }
