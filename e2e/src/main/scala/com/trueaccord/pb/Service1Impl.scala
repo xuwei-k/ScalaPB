7d3f0e5c998c04c6668cf74ca714c5b642548c93
xuwei-k <6b656e6a69@gmail.com>
2015-11-18 20:46:01 +0900

    more grpc sample and test

--- e2e/src/main/scala/com/trueaccord/pb/Service1Impl.scala
+++ e2e/src/main/scala/com/trueaccord/pb/Service1Impl.scala
@@ -1,14 +1,54 @@
 package com.trueaccord.pb
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import com.trueaccord.proto.e2e.service.Service1Grpc.Service1
-import com.trueaccord.proto.e2e.service.{Req1, Req2, Res1, Res2}
+import com.trueaccord.proto.e2e.service._
+import io.grpc.stub.StreamObserver
 
 import scala.concurrent.Future
 
-object Service1Impl extends Service1[Future]{
-  override def hello(request: Req1): Future[Res1] =
-    Future.successful(Res1.defaultInstance)
+object Service1Impl {
 
-  override def foo(request: Req2): Future[Res2] =
-    Future.successful(Res2.defaultInstance)
+  val method3Limit = 50
+
+}
+
+class Service1Impl extends Service1[Future]{
+
+  override def method1(request: Req1): Future[Res1] =
+    Future.successful(Res1(length = request.request.length))
+
+  override def method2(observer: StreamObserver[Res2]) =
+    new StreamObserver[Req2] {
+      private[this] val counter = new AtomicInteger()
+      override def onError(e: Throwable): Unit =
+        observer.onError(e)
+
+      override def onCompleted(): Unit = {
+        observer.onNext(Res2(counter.getAndSet(0)))
+      }
+
+      override def onNext(v: Req2): Unit = {
+        counter.incrementAndGet()
+      }
+    }
+
+  private[this] var method3Counter = 0
+
+  override def method3(request: Req3, observer: StreamObserver[Res3]): Unit = synchronized{
+    method3Counter += request.num
+    if(method3Counter > Service1Impl.method3Limit){
+      observer.onNext(Res3())
+      observer.onCompleted()
+    }
+  }
+
+  // TODO bidirectional streaming sample and test
+  override def method4(observer: StreamObserver[Res4]): StreamObserver[Req4] =
+    new StreamObserver[Req4] {
+      override def onError(e: Throwable): Unit = {}
+      override def onCompleted(): Unit = {}
+      override def onNext(request: Req4): Unit = {}
+    }
 }
--- e2e/src/test/scala/ServiceSpec.scala
+++ e2e/src/test/scala/ServiceSpec.scala
@@ -1,12 +1,15 @@
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.{TimeUnit, TimeoutException}
 
 import com.trueaccord.pb.Service1Impl
 import com.trueaccord.proto.e2e.service._
 import io.grpc.netty.{NegotiationType, NettyChannelBuilder, NettyServerBuilder}
+import io.grpc.stub.StreamObserver
 import io.grpc.{ManagedChannel, ServerServiceDefinition}
 import org.scalatest.FlatSpec
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.concurrent.{Future, Await, ExecutionContext, Promise}
+import scala.util.Random
 
 class ServiceSpec extends FlatSpec {
 
@@ -15,11 +18,76 @@ class ServiceSpec extends FlatSpec {
     override def execute(runnable: Runnable): Unit = runnable.run()
   }
 
-  "Service" should "work" in {
-    withServer(Service1Grpc.bindService(Service1Impl, singleThreadExecutionContext)){ channel =>
+  private[this] def getObserverAndFuture[A]: (StreamObserver[A], Future[A]) = {
+    val promise = Promise[A]()
+    val observer = new StreamObserver[A] {
+      override def onError(t: Throwable): Unit = {}
+      override def onCompleted(): Unit = {}
+      override def onNext(value: A): Unit = promise.success(value)
+    }
+    (observer, promise.future)
+  }
+
+  private[this] def randomString(): String = Random.alphanumeric.take(Random.nextInt(10)).mkString
+
+  "method1 blockingClient" should "return request string length" in {
+    withServer(Service1Grpc.bindService(new Service1Impl, singleThreadExecutionContext)){ channel =>
       val client = Service1Grpc.blockingClient(channel)
-      client.hello(Req1())
-      client.foo(Req2())
+      val string = randomString()
+      assert(client.method1(Req1(string)).length === string.length)
+    }
+  }
+
+  "method1 futureClient" should "return request string length" in {
+    withServer(Service1Grpc.bindService(new Service1Impl, singleThreadExecutionContext)){ channel =>
+      val client = Service1Grpc.futureClient(channel)
+      val string = randomString()
+      assert(Await.result(client.method1(Req1(string)), 3.seconds).length === string.length)
+    }
+  }
+
+  "method2" should "return message count" in {
+    withServer(Service1Grpc.bindService(new Service1Impl, singleThreadExecutionContext)){ channel =>
+      val client = Service1Grpc.futureClient(channel)
+      val (responseObserver, future) = getObserverAndFuture[Res2]
+      val requestObserver = client.method2(responseObserver)
+      val n = Random.nextInt(10)
+      for(_ <- 1 to n) {
+        requestObserver.onNext(Req2())
+      }
+
+      try{
+        Await.result(future, 3.seconds)
+        fail("unexpected")
+      }catch{
+        case _: TimeoutException =>
+      }
+
+      requestObserver.onCompleted()
+      assert(Await.result(future, 3.seconds).count === n)
+    }
+  }
+
+  "method3" should "return response when reach limit" in {
+    withServer(Service1Grpc.bindService(new Service1Impl, singleThreadExecutionContext)) { channel =>
+      val client = Service1Grpc.futureClient(channel)
+      val (observer, future) = getObserverAndFuture[Res3]
+      val requests = Stream.continually(Req3(num = Random.nextInt(10)))
+      val count = requests.scanLeft(0)(_ + _.num).takeWhile(_ < Service1Impl.method3Limit).size - 1
+
+      requests.take(count).foreach{ req =>
+        client.method3(req, observer)
+      }
+
+      try{
+        Await.result(future, 3.seconds)
+        fail("unexpected")
+      }catch{
+        case _: TimeoutException =>
+      }
+
+      client.method3(Req3(1000), observer)
+      Await.result(future, 3.seconds)
     }
   }
 
