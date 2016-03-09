622c120a277a89d883802e1fa77fe26902c3e33f
xuwei-k <6b656e6a69@gmail.com>
2016-03-09 14:05:36 +0900

    test

--- e2e.sh
+++ e2e.sh
@@ -3,5 +3,23 @@ set -e
 sbt ++2.10.6 compilerPlugin/publishLocal runtimeJVM/publishLocal createVersionFile \
     ++2.11.7 runtimeJVM/publishLocal grpcRuntime/publishLocal
 cd e2e
-sbt clean test
+sbt clean test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test &&
+sbt test
 
