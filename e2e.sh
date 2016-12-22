9b8c2efa2fd371d4f852c68cbe89c89521929075
xuwei-k <6b656e6a69@gmail.com>
2016-12-22 15:23:41 +0900

    wip

--- e2e.sh
+++ e2e.sh
@@ -3,7 +3,7 @@ set -e
 sbt ++2.10.6 compilerPlugin/publishLocal runtimeJVM/publishLocal createVersionFile \
     ++2.11.8 runtimeJVM/publishLocal grpcRuntime/publishLocal
 cd e2e
-for t in List Vector Array
+for t in Seq scala.collection.immutable.Seq scala.collection.immutable.IndexedSeq List Vector
 do
   sbt -DcollectionType=${t} clean compile
 done
