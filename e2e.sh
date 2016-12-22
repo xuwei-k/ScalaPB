77e0b2fc4314502cdf5b6b629eb52060bcf2cbd4
xuwei-k <6b656e6a69@gmail.com>
2016-12-22 18:10:13 +0900

    wip

--- e2e.sh
+++ e2e.sh
@@ -3,9 +3,9 @@ set -e
 sbt ++2.10.6 compilerPlugin/publishLocal runtimeJVM/publishLocal createVersionFile \
     ++2.11.8 runtimeJVM/publishLocal grpcRuntime/publishLocal
 cd e2e
-for t in Seq scala.collection.immutable.Seq scala.collection.immutable.IndexedSeq List Vector
+for t in scala.collection.immutable.Seq scala.collection.immutable.IndexedSeq List Vector
 do
-  sbt -DcollectionType=${t} clean compile
+  sbt -DcollectionType=${t} clean compile test
 done
 
 sbt clean noJava/clean noJava/test test
