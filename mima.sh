e5ab08311d3fc6fd0007dfc7fd71da2a06da2197
xuwei-k <6b656e6a69@gmail.com>
2019-06-14 15:13:25 +0900

    enable mima for Scala 2.13

--- mima.sh
+++ mima.sh
@@ -2,7 +2,7 @@
 set -e
 SCALA_VERSION=${SCALA_VERSION:-${TRAVIS_SCALA_VERSION:-2.11.11}}
 
-if [ "$SCALA_VERSION" != "2.13.0" ]
+if [[ "$SCALA_VERSION" != "2.14."* ]]
 then    sbt ++$SCALA_VERSION \
         grpcRuntime/mimaReportBinaryIssues \
         lensesJVM/mimaReportBinaryIssues \
